dc4b2a4ffd545519a39d537b6ae600007694b5c2
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-09-04 09:37:26 -0500

    String Exercises Started

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log('Script Running');
 
+// ARRAYS //
+
+// Exercise #1
 // Name two dynamic ways to add the value of “bar” to the end of array “foo”.
 
 let foo = [1, 2, 3];
@@ -21,4 +24,42 @@ function concatFooToBar(bar) {
     return newFoo;
 };  // end function concatFooToBar
 
+// Exercise #2
+// Given an array of numbers, create toPalindrome method that creates a 
+// palindrome out of your array in the following way:
 
+let obj = {
+    toPalindrome: function(){
+        return
+    }
+}
+
+const arr = [1, 2, 3];
+// const arr2 = arr.toPalindrome();
+// // [1,2,3,2,1]
+
+// const arr3 = arr2.toPalindrome();
+// // [1,2,3,2,1,2,3,2,1]
+
+
+// STRINGS //
+
+// Exercise #1
+
+// What do each of the following expressions equate to, and why?
+"2" === 2;
+"2" == 2;
+
+// “2” === 2 equates to a falsy value.The === can be used to check if two values are 
+// both equal and of the same type.For the two values to be truthy with respect to one 
+// another, they must both have the same value, and either both be numbers or both be 
+// strings.In this example, the values are the same since they are both 2, but the types 
+// are different since one is a string(“2”) and the other is a number(2).
+
+// “2” == 2 equates to a truthy value.The == can be used to check if two values are both 
+// equal to each other regardless of their type.For the two values to be falsy with 
+// respect to one another, they must have two different values, such as 2 and 3.  The 
+// fact that the two values are not of the same type is not important when using the 
+// == symbol.In this example, the values are the same since they are both 2, and the 
+// statement remains truthy despite the fact that one is a string and the other is a 
+// number.
